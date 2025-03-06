335b7a7a51ab85145a3d5030752eb483b1fbdd53
Ifra01 <jamalifra74@gmail.com>
2025-03-06 23:45:51 +0500

    created app.js

--- chapter 35-38.js
+++ chapter 35-38.js
@@ -143,3 +143,6 @@
 // string(word);
 
 // Question 13 Write a JavaScript function that accepts two arguments, a string and a letter and the function will count the number of occurrences of the specified letter within the string.  Sample arguments : 'JSResourceS.com', 'o' .
+
+
+// Question 14
